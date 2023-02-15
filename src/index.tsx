78f12bcf347fa35d89aaaecab51ce4b5a8359698
Ian Oliveira <ian.oliveira@einstein.br>
2023-02-14 23:06:17 -0300

    refactor: remove components initial for default project

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import 'index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -14,7 +14,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
