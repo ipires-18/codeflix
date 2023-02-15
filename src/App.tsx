d421565d2c5f6c2128baa45b63d9444de0b03065
Ian Oliveira <ian.oliveira@einstein.br>
2023-02-15 00:29:11 -0300

    feat: add logic config navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import { ThemeProvider, Box } from '@mui/system';
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+import { Routes, Route } from 'react-router-dom';
+
+export const Home = (): JSX.Element => {
+  return <div>Home</div>;
+};
+
+export const About = (): JSX.Element => {
+  return <div>About</div>;
+};
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={{}}>
-      <Box component="main" sx={{ height: '100vh', background: '#000' }} />
+    <ThemeProvider theme={appTheme}>
+      <Box
+        component="main"
+        sx={{ height: '100vh', background: theme => theme.palette.grey[900] }}
+      >
+        <Header />
+        <Layout>
+          <h1>Olá mundo</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Layout>
+      </Box>
     </ThemeProvider>
   );
 }
